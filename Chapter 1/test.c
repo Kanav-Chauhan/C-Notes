7e9364769e063758971b704d874934d6f918e572
Kanav <kanavchauhan001@gmail.com>
2022-03-11 12:53:26 +0530

    Commit 2

---
+++ Chapter 1/test.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+
+// int main (){
+
+//     int r ;
+//     float pi;
+//     pi = 3.14;
+
+    
+//     scanf("%d",&r);
+
+    
+//     printf(" area %.2f ",(pi*r*r)) ; 
+
+
+//     return 0;
+
+// }   
+
+
+
+
+
+
